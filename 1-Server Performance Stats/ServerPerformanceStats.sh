10a240f04277f58c53ec20eadf5025c948e1638a
hliblack <hliblack@outlook.com>
2025-10-08 23:18:50 +0800

    1-Server Performance Stats，增加获取TOP5进程信息

--- 1-Server Performance Stats/ServerPerformanceStats.sh
+++ 1-Server Performance Stats/ServerPerformanceStats.sh
@@ -17,3 +17,11 @@ echo "Swap Usage: $SWAP_USAGE MB"
 # Get disk usage
 DISK_USAGE=$(df -h | grep "/dev/sda1" | awk '{print $5}')
 echo "Disk Usage: $DISK_USAGE"
+
+# Get Top5 CPU-consuming processes
+TOP5_PROCESSES=$(top -b -n 1 | awk 'NR>7 {print $0}' | sort -k9,9nr | head -n 5)
+echo "Top 5 CPU-consuming processes: $TOP5_PROCESSES"
+
+# Get Top5 Memory-consuming processes
+TOP_PROCESSES=$(top -b -n 1 | awk 'NR>7 {print $0}' | sort -k10,10nr | head -n 5)
+echo "Top 5 Memory-consuming processes: $TOP_PROCESSES"
